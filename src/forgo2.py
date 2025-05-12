576a2f5a131ff09255646a166e802e025f121d9a
timm <timm@ieee.org>
2025-05-12 06:39:26 -0400

    saving

--- src/forgo2.py
+++ src/forgo2.py
@@ -20,84 +20,108 @@ from collections import defaultdict
 sys.dont_write_bytecode = True
 BIG = 1E32
 
-#------------------------------------------------------------------------------
 class o: 
-  __init__ = lambda i,**d : i.__dict__.update(**d)
-  __repr__ = lambda i     : i.__class__.__name__ + '('+str(i.__dict__)+')'
+  __init__ = lambda i, **d: i.__dict__.update(d)
+  __repr__ = lambda i: return i.__class__.__name__+'('+show(i.__dict__)+')'
+
+def show(d):
+  return str({k: (f"{v:3g}" if type(v)==float else v) for k,v in d.items()})
 
 #------------------------------------------------------------------------------
-class Data:
-  def __init__(i, names, rows=[]):
-    """Numeric columns have `lo` and `hi`. Y-values have `goal`.
-    `f` is where we store frequencies f[col][bin]."""
-    i.names, i.hi, i.lo, i.goal, i.rows = names, {}, {}, {}, []
-    i.f = lambda: defaultdict(lambda: defaultdict(int))
-    for c,s in enumerate(names):
-      if s[0].isupper():
-        i.hi[c], i.lo[c] = -BIG, BIG
-        if s[-1] in "+-":
-          i.goal[c] = 0 if s[-1]=='-' else 1
-    i.adds(rows)
- 
-  def clone(i, rows=[]): return Data(i.names, rows)
+class Num(o):
+  def __init__(i, has=[], txt=" "): 
+    i.txt=txt
+    i.sd = i.m2 = i.mu = i.n = 0
+    [i.add(x) for x in has]
+  
+  def bin(i,x) : return min(the.bins-1, int(i.cdf(x)*the.bin))
+  def norm(i,x): return (x - i.lo) / (i.hi - i.lo + 1/BIG)
+  def sub(i,x) : return i.add(x,-1)
 
-  def adds(i,rows=[]): 
-    "Add multiple rows, update the frequency counts."
+  def add(i,x,inc=1):
+    i.n += inc
+    i.lo = min(v, i.lo)
+    i.hi = max(v, i.hi)
+    if inc < 0 and i.n < 2: 
+      i.n = i.mu = i.m2 = i.sd = 0
+    else:
+      d = i.mu - x
+      i.mu += inc * d/i.n
+      i.m2 += inc * d * (x - i.mu)
+      i.sd  = 0 if i.n <=2  else (max(0,i.m2)/(i.n-1))**.5
+    return x
+
+  def cdf(i,x):
+    def fun(z): return 1 - 0.5 * math.exp(-0.717 * z - 0.416 * z * z) 
+    z = (x - i.mu) / i.sd
+    return  z>=0 and fun(z) or 1 - fun(-z) end
+
+class Data(o):
+  def __init__(i, rows):
+    rows = iter(rows)
+    i.names, i.nums, i.goals, i.rows = next(rows), {}, {}, []
+    i.header()
     [i.add(row) for row in rows]
-    i.counts()
+
+  def header(i):
+    i.f = None
+    for c,s in enumerate(i.names):
+      if s[0].isupper():
+        i.nums[c] = Num() # hi lo
+        if s[-1] in "+-":
+          i.goals[c] = 0 if s[-1]=='-' else 1
+
+  def clone(i, rows=[]): return Data([i.names] + rows)
 
   def add(i, row):
-    "Add one rows, update the lo and hi knowledge."
-    i.rows += [row]
-    i.bins += [row[:]]
-    for c in i.hi:
+    "Add one row, update the num knowledge."
+    for c,n in i.nums.items():
       x = row[c]
       if x != "?":
-        row[c]  = x = float(x)
-        i.lo[c] = min(i.lo[c], x)
-        i.hi[c] = max(i.hi[c], x)
+        row[c] = n.add(float(x))
+    i.rows += [row]
+    i.f = None
+    return row
+
+  def ok(i):
+    "Fill in the bins and the frequency counts."
+    if not i.f:
+      i.f  = lambda: defaultdict(lambda: defaultdict(int))
+      for row in i.rows:
+        for c,x in enumerate(row):
+          if x != "?": 
+            i.f[c][ i.bin(c,x) ] += 1
     return i
 
-  def counts(i):
-    "Fill in the bins and the frequency counts."
-    for rows in i.rows:
-      for c,x in enumerate(i.rows):
-        if x != "?" and c not in i.goal:
-          i.f[c][ i.bin(c,x) ] += 1
-
   def like(i, row, nh, nall):
-    def fun(c,b):
-      return (i.f[c][b] + the.m*prior) / (len(i.rows) + the.m + 1/BIG)
+    def like1(c,b):
+      return (f[c][b] + the.m*prior) / (len(i.rows) + the.m + 1/BIG)
+
+    f     = i.ok().f
     prior = (len(i.rows) + the.k) / (nall + the.k*nh)
-    tmp   = [fun(c, i.bin(c,x)) for c,x in enumerate(row) 
-             if x != "?" and c not in goal]
+    tmp   = [like1(c, i.bin(c,x)) for c,x in enumerate(row) 
+             if x != "?" and c not in goals]
     return sum(math.log(n) for n in tmp + [prior] if n>0)
 
   def ydist(i,row):
-    n = sum(abs(i.norm(c, row[c]) - g) ** the.p for c,g in i.goals.items())
-    return (n / len(d.goal)) ** (1 / the.p)
+    n= sum(abs(i.nums[c].norm(row[c]) - g) ** the.p for c,g in i.goals.items())
+    return (n / len(d.goals)) ** (1 / the.p)
 
-  def rorder(i):
-    rows.sort(key=lambda r: i.ydist(r))
+  def sort(i):
+    rows.sort(key=lambda row: i.ydist(row))
     return i
 
-  def norm(i,c,x):
-    if x=="?" or c not in hi: return x
-    return (x - i.lo[c]) / (i.hi[c] - i.lo[c] + 1/BIG)
-
-  def bin(i,c,x):
-    if x=="?" or c not in hi: return x
-    return min(the.bin - 1, int(i.norm(c,x)*the.bin))
+  def bin(i,c,x): return i.nums[c].bins(c) if c in i.nums else x
 
 #------------------------------------------------------------------------------
 def actLearn(d):
   random.shuffle(d.rows)
   n    = the.start
   m    = round(n**the.guess)
-  done = d.clone(d.rows[:n]).rorder()
   todo = d.rows[n:]
-  best = d.clone(done.rows[:m])
-  rest = d.clone(done.rows[m:])
+  labeled = d.clone(d.rows[:n]).sort()
+  best = d.clone(labeled.rows[:m])
+  rest = d.clone(labeled.rows[m:])
   while len(todo) > 2 and m < the.Stop:
     n += 1
     hi, *lo = sorted(todo[:the.Few], key=_guess, reverse=True)
@@ -105,20 +129,20 @@ def actLearn(d):
     best.add(hi)
     br.add(hi)
     if len(best.rows) >= round(n**the.guess):
-      rest.add( best.sub( best.rorder().rows.pop(-1)))
+      rest.add( best.sub( best.sort().rows.pop(-1)))
   return o(best=best, rest=rest, todo=todo)
 
 #------------------------------------------------------------------------------
 def csv(file=None):
   for line in fileinput.input(file):
-    if line := line.split("#")[0].replace(" ", "").strip():
-      yield [x for x in line.split(",")]
+    if line:
+      yield [x.strip() for x in line.split(",")]
 
-def cli(d, args):
-  for c,arg in enumerate(args):
+def cli(d):
+  for c,arg in enumerate(sys.argv):
     for k,v in d.items():
       if arg == "-"+k[0]:  
-        d[k] = coerce(args[c+1] if c < len(args) - 1 else str(v))
+        d[k] = coerce(sys.argv[c+1])
 
 #------------------------------------------------------------------------------
 def eg_h(_): print(__doc__)
@@ -129,7 +153,7 @@ the = o(**{m[1]:coerce(m[2])
         for m in re.finditer(r"-\w+\s*(\w+).*=\s*(\S+)", __doc__)})
 
 if __name__ == "__main__":
-  cli(the.__dict__, sys.argv) # update 'the' from the command line
+  cli(the.__dict__) # update 'the' from the command line
   for n,s in enumerate(sys.argv):
     if fun := globals().get("eg" + s.replace("-","_")):
       random.seed(the.rseed) # reset the random seed before running an eg
